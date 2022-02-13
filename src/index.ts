76b339e8063f83aa701d8729a31927dd78aad028
Harrison Freni <hfreni@hfreni.com>
2022-02-13 00:18:27 -0500

    Took some notes on OTP

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+// OTP Structure Info:
+// PDU (Protocol Data Unit) cointains 3 segments:
+// 1. Vector Field, 2 Octets; A key that describes the format
+//    of the info in data field
+// 2. Length Field, 2 Octets; Specifies the length in octets of
+//    the rest of the PDU, excl. Vector & Length fields.
+// 3. Data Field, Payload, Variable Octets; can be empty, contain
+//    sub-fields, or whole entire PDUs.
+
+// The OTP Layer
+// Octets, Field Size, Field Name, Decription + Content.
+// 0-11, 12, OTP Packet Identifier (I AM OTP);
+// 0x4f 0x54 0x50 0x2d 0x45 0x31 0x2e 0x35 0x39 0x00 0x00 0x00
+
+// 12-13, 2, Vector, Identifies type of OTP data in PDU.
+// VECTOR_OTP_TRANSFORM_MESSAGE, VECTOR_OTP_ADVERTISEMENT_MESSAGE
+
+// 14-15, 2, Length, Length of Message excl. Footer starting w/ Oct. 16
+// 16, 1, Footer Options, Reserved, transmitted as 0x00
+// 17, 1, Footer Length, Length of Footer in Octets.
+// 18-33, 16, CID, Sender's CID (Unique ID).
+// 34-37, 4, Folio #, Indicates which folio this is.
+// 38-39, 2, Page, Which page of 0 thru N this is.
+// 40-41, 2, Last Page, indicates which page N this is.
+// 42, 1, Options,  Reserved 0x00
+// 43-46, 4, Reserved, 0x00000000
+// 47-78, 32, Component Name, Human Readable UTF-8 String for Component
+
+// Producer = Tracker
+// Consumer = Console, WebApp.
+
+// Transform Message:
+// Octets, Field Size, Field Name, Decription + Content.
+
+
+// Advertisement Message
+// Octets, Field Size, Field Name, Decription + Content.
+// [OTP LAYER, Octets 0-78]
